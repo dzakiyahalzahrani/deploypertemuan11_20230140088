05ce237038ac690dffcb74fe4ee54335d107f4a3
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-05-18 10:59:29 +0700

    mengisi code untuk Profile.java

--- src/main/java/com/example/pertemuan11/model/Profile.java
+++ src/main/java/com/example/pertemuan11/model/Profile.java
@@ -1,4 +1,26 @@
 package com.example.pertemuan11.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.swing.*;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+    @Id
+    private Spring id;
+    private Spring nama;
+    private Spring alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
