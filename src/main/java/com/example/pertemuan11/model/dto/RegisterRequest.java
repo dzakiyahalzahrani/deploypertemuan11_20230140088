f325143444c48d09905e6b67a51fae2231baed26
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-05-18 11:02:05 +0700

    mengisi code untuk RegisterRepository.java

--- src/main/java/com/example/pertemuan11/model/dto/RegisterRequest.java
+++ src/main/java/com/example/pertemuan11/model/dto/RegisterRequest.java
@@ -1,4 +1,15 @@
 package com.example.pertemuan11.model.dto;
 
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
 public class RegisterRequest {
+    private Spring username;
+    private Spring password;
+
+    private Spring nama;
+    private Spring alamat;
+
 }
