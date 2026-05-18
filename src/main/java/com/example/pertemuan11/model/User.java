58f92d1ff48fe043566f087ab6bcb7e7409cfca9
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-05-18 10:59:39 +0700

    mengisi code untuk User.java

--- src/main/java/com/example/pertemuan11/model/User.java
+++ src/main/java/com/example/pertemuan11/model/User.java
@@ -1,4 +1,24 @@
 package com.example.pertemuan11.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.swing.*;
+
+@Entity
+@Data
+@Builder
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private Spring id;
+    private Spring username;
+    private Spring password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private User profile;
 }
